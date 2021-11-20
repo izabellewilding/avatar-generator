45cd49bf50d8b5c08761f0155f12a460af786127
Izabelle Wilding <izabellewilding@gmail.com>
2021-11-20 23:41:57 +0000

    WIP

--- src/index.js
+++ src/index.js
@@ -1,24 +1,22 @@
+const mergeImages = require("merge-images");
+const { Canvas, Image } = require("canvas");
+const path = require("path");
+const fs = require("fs");
 
-const mergeImages = require('merge-images');
-const { Canvas, Image } = require('canvas');
-const path = require('path');
-const fs = require('fs');
-
-//joining path of directory 
-const clothes = path.join(__dirname, 'assets/clothes');
-const eyebrow = path.join(__dirname, 'assets/eyebrow');
-const eyes = path.join(__dirname, 'assets/eyebrow');
-const mouth = path.join(__dirname, 'assets/mouth');
-const nose = path.join(__dirname, 'assets/node');
-const top = path.join(__dirname, 'assets/top')
-
+//joining path of directory
+// const clothes = path.join(__dirname, 'assets/clothes');
+// const eyebrow = path.join(__dirname, 'assets/eyebrow');
+// const eyes = path.join(__dirname, 'assets/eyebrow');
+// const mouth = path.join(__dirname, 'assets/mouth');
+// const nose = path.join(__dirname, 'assets/node');
+// const top = path.join(__dirname, 'assets/top')
 
 //passsing directoryPath and callback function
 // fs.readdir(clothes, function (err, files) {
 //     //handling error
 //     if (err) {
 //         return console.log('Unable to scan directory: ' + err);
-//     } 
+//     }
 //         // Randomly choose an image from the folder
 //     const chosenFile = files[Math.floor(Math.random() * files.length)]
 
@@ -28,36 +26,51 @@ const top = path.join(__dirname, 'assets/top')
 //       })
 //         .then(b64 => console.warn(b64));
 //         // data:image/png;base64,iVBORw0KGgoAA...
-      
-//     console.log(chosenFile); 
+
+//     console.log(chosenFile);
 // });
 
+const folderNames = ["clothes", "eyebrow"];
+
+const folderPaths = folderNames.map((folderName) => {
+  return path.join(__dirname, `assets/${folderName}`);
+});
 
 function selectAssets(folder) {
-    fs.readdir(folder, function (err, files) {
+  const files = fs.readdirSync(folder) 
+      // Randomly choose an image from the folder
+    const chosenFile = files[Math.floor(Math.random() * files.length)];
 
-        //handling error
-        if (err) {
-            return console.log('Unable to scan directory: ' + err);
-        } 
-            // Randomly choose an image from the folder
-        const chosenFile = files[Math.floor(Math.random() * files.length)]
+    console.warn("chosen file", chosenFile);
+    return chosenFile
+  
+//   function (err, files) {
+//     //handling error
+//     if (err) {
+//       return console.log("Unable to scan directory: " + err);
+//     }
 
-        mergeImages( [`${folder}/${chosenFile}`], {
-            Canvas: Canvas,
-            Image: Image
-          })
-            .then(b64 => console.warn(b64));
-            // data:image/png;base64,iVBORw0KGgoAA...
-        console.log(chosenFile); 
-    });
+//   });
 }
 
-const selectedClothes = selectAssets(clothes)
-const selectedEyebrows = selectAssets(eyebrow)
+const arr = [];
+
+const assets = folderPaths.map((folderPath) => selectAssets(folderPath));
+
+console.log(assets, folderPaths,  "assets");
+
+// const selectedEyebrows = selectAssets(eyebrow)
+
+// console.log(selectedClothes, 'selected');
+
+mergeImages(assets, {
+    Canvas: Canvas,
+    Image: Image
+  })
+    .then(b64 => console.warn(b64));
+
+// console.log(selectedClothes, 'selected clothes ')
 // selectAssets(eyes)
 // selectAssets(mouth)
 // selectAssets(nose)
 // selectAssets(top)
-
-
