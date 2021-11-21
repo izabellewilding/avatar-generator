3700b3e18f1d33c70d342734a48e5248e15cee02
Izabelle Wilding <izabellewilding@gmail.com>
2021-11-21 00:26:42 +0000

    WIP

--- src/index.js
+++ src/index.js
@@ -3,30 +3,44 @@ const { Canvas, Image } = require("canvas");
 const path = require("path");
 const fs = require("fs");
 
-const folderNames = ["clothes", "eyebrow", "eyes", "mouth", "nose", "top"];
+const folderNames = [
+  "skin",
+  "eyebrow",
+  "eyes",
+  "mouth",
+  "nose",
+  "top",
+  "clothes",
+];
 
 const folderPaths = folderNames.map((folderName) => {
   return path.join(__dirname, `assets/${folderName}`);
 });
 
 function selectAssets(folder) {
-  const files = fs.readdirSync(folder) 
-      // Randomly choose an image from the folder
-    const chosenFile = files[Math.floor(Math.random() * files.length)];
-    return chosenFile
+  const files = fs.readdirSync(folder);
+  // Randomly choose an image from the folder
+  const chosenFile = files[Math.floor(Math.random() * files.length)];
+  return chosenFile;
 }
 
-const selectedAssetsPaths = folderPaths.map((folderPath) => {const asset = selectAssets(folderPath)
-return `${folderPath}/${asset}`
+const selectedAssetsPaths = folderPaths.map((folderPath) => {
+  const asset = selectAssets(folderPath);
+  return `${folderPath}/${asset}`;
 });
 
 mergeImages(selectedAssetsPaths, {
-    Canvas: Canvas,
-    Image: Image
-  })
-    .then(b64 => console.warn(b64));
+  Canvas: Canvas,
+  Image: Image,
+}).then((b64) => {
+  console.warn(b64);
+  
+  const base64Data = b64.replace(/^data:image\/png;base64,/, "");
 
+  fs.writeFile("out.png", base64Data, "base64", function (err) {
+    console.log(err);
+  });
+});
 
-//TODO: Output number of avatars into folder at a time to display on web page 
-
-
+//TODO: Output number of avatars into folder at a time to display on web page
+//TODO: Skin & BGs
