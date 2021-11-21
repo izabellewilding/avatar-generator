1dc1e8fcb4abe1adedf62cc356df6de8fe65abcc
Izabelle Wilding <izabellewilding@gmail.com>
2021-11-21 00:32:59 +0000

    WIP

--- src/index.js
+++ src/index.js
@@ -17,30 +17,43 @@ const folderPaths = folderNames.map((folderName) => {
   return path.join(__dirname, `assets/${folderName}`);
 });
 
-function selectAssets(folder) {
-  const files = fs.readdirSync(folder);
-  // Randomly choose an image from the folder
-  const chosenFile = files[Math.floor(Math.random() * files.length)];
-  return chosenFile;
+function generateRandomImage(id) {
+  function selectAssets(folder) {
+    const files = fs.readdirSync(folder);
+    // Randomly choose an image from the folder
+    const chosenFile = files[Math.floor(Math.random() * files.length)];
+    return chosenFile;
+  }
+
+  const selectedAssetsPaths = folderPaths.map((folderPath) => {
+    const asset = selectAssets(folderPath);
+    return `${folderPath}/${asset}`;
+  });
+
+  mergeImages(selectedAssetsPaths, {
+    Canvas: Canvas,
+    Image: Image,
+  }).then((b64) => {
+    console.warn(b64);
+
+    const base64Data = b64.replace(/^data:image\/png;base64,/, "");
+
+    fs.writeFile(
+      __dirname + `/output/avatar-${id}.png`,
+      base64Data,
+      "base64",
+      function (err) {
+        console.log(err);
+      }
+    );
+  });
 }
 
-const selectedAssetsPaths = folderPaths.map((folderPath) => {
-  const asset = selectAssets(folderPath);
-  return `${folderPath}/${asset}`;
-});
-
-mergeImages(selectedAssetsPaths, {
-  Canvas: Canvas,
-  Image: Image,
-}).then((b64) => {
-  console.warn(b64);
-  
-  const base64Data = b64.replace(/^data:image\/png;base64,/, "");
-
-  fs.writeFile("out.png", base64Data, "base64", function (err) {
-    console.log(err);
+Array(25)
+  .fill()
+  .map((_, arrayIndex) => {
+    generateRandomImage(arrayIndex);
   });
-});
 
 //TODO: Output number of avatars into folder at a time to display on web page
 //TODO: Skin & BGs
